56e658fd4f6e32ee0d934467737ef8fdac8137d8
lfpicoloto1 <133693873+lfpicoloto1@users.noreply.github.com>
2023-05-15 15:21:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask
+app = Flask(__name__)
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
 
-        message = "Hello, World! Here is a GET response"
-        self.wfile.write(bytes(message, "utf8"))
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-
-with HTTPServer(('', 8000), handler) as server:
-    server.serve_forever()
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Hello world!'
